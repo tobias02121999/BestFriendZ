2da3b60339668acc86583286c65087edfaa9bcac
tobias02121999 <hedgefoggames@outlook.com>
2018-08-11 00:05:51 +0200

    Update 0.4
    // Unity
    - Added basic gameplay mechanics

---
+++ BestFriendZ/Assets/scr_gameControllerZombifier.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_gameControllerZombifier : MonoBehaviour {
+
+    // Initialize the public variables
+    public GameObject[] players;
+    public Text timerText;
+    public Text gameOverText;
+    public float timerDuration;
+    public Transform pointLightPivotTransform;
+
+    // Initialize the private variables
+    private float timer;
+    private int i;
+    private bool gameOver;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Choose one random player and turn him into a zombie
+        players[Mathf.RoundToInt(Random.Range(0f, players.Length - 1f))].GetComponent<scr_playerMain>().isZombie = true;
+
+        // Set the timer to equal the given timer duration
+        timer = timerDuration;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Decrease the timer duration over time in seconds
+        timer -= Time.deltaTime;
+
+        if (!gameOver)
+            timerText.text = Mathf.Round(timer).ToString();
+        else
+            timerText.text = "";
+
+        // Check for human and zombie win conditions if a game over has not yet been triggered
+        if (!gameOver)
+        {
+            // Check if the timer has reached 0
+            if (timer <= 0f)
+            {
+                // The humans have won the match
+                gameOverText.text = "humans win!";
+                gameOver = true;
+            }
+
+            // Check if there are still humans left
+            if (i < players.Length)
+            {
+                // If the current player being checked is human, reset the indicator
+                if (players[i].GetComponent<scr_playerMain>().isZombie == false)
+                    i = 0;
+                else
+                    i++;
+            }
+            else
+            {
+                // The zombies have won the match
+                gameOverText.text = "zombies win!";
+                gameOver = true;
+            }
+        }
+
+        float value = 270 / (timerDuration / Time.deltaTime);
+        pointLightPivotTransform.Rotate(value, 0f, 0f);
+	}
+}
---
+++ BestFriendZ/Assets/scr_playerMain.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_playerMain : MonoBehaviour {
+
+    // Initialize the public variables
+    public int playerID;
+    public bool isZombie;
+    public float movementSpeed;
+    public float movementSpeedZombie;
+    public Transform cameraTransform;
+    public GameObject playerHumanModel;
+    public GameObject playerZombieModel;
+
+    // Initialize the private variables
+    private float movementAxisX;
+    private float movementAxisY;
+    private float rotationY;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Store the player input in variables
+        switch (playerID)
+        {
+            case 0:
+                movementAxisX = Input.GetAxis("P0 Horizontal");
+                movementAxisY = Input.GetAxis("P0 Vertical");
+                break;
+
+            case 1:
+                movementAxisX = Input.GetAxis("P1 Horizontal");
+                movementAxisY = Input.GetAxis("P1 Vertical");
+                break;
+
+            case 2:
+                movementAxisX = Input.GetAxis("P2 Horizontal");
+                movementAxisY = Input.GetAxis("P2 Vertical");
+                break;
+
+            case 3:
+                movementAxisX = Input.GetAxis("P3 Horizontal");
+                movementAxisY = Input.GetAxis("P3 Vertical");
+                break;
+        }
+
+        // Change the players mesh to make him look like a zombie, if necessary
+        if (isZombie && !playerZombieModel.activeSelf)
+        {
+            playerHumanModel.SetActive(false);
+            playerZombieModel.SetActive(true);
+            movementSpeed = movementSpeedZombie;
+        }
+    }
+
+    // FixedUpdate is called once per fixed frame
+    void FixedUpdate()
+    {
+        // Move around according to the player input
+        transform.position += cameraTransform.right * movementSpeed * movementAxisX * Time.deltaTime;
+        transform.position += cameraTransform.forward * movementSpeed * movementAxisY * Time.deltaTime;
+
+        if (movementAxisX != 0f || movementAxisY != 0f)
+            rotationY = Mathf.Atan2(movementAxisX, movementAxisY);
+
+        transform.rotation = Quaternion.Euler(0f, rotationY * Mathf.Rad2Deg, 0f);
+    }
+
+    // OnCollisionEnter is called once collision is detected
+    void OnCollisionEnter(Collision collision)
+    {
+        if (isZombie && !collision.gameObject.GetComponent<scr_playerMain>().isZombie)
+            collision.gameObject.GetComponent<scr_playerMain>().isZombie = true;
+    }
+}
